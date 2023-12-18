1ea2acd09c203b8b3ffb8e245a17e2d1fd87c56c
Gaurav Gurjar <ggurjar333@gmail.com>
2023-12-18 12:11:14 +0530

    creates logger and current_season_schedule.py

---
+++ src/sportsradar/extract/current_season_schedule.py
@@ -0,0 +1,9 @@
+from sportsradar.logging_helpers
+
+logger = sportsradar.logging_helpers.get_logger(__name__)
+
+class CurrentSeasonSchedule:
+    def __init__(self, current_season_schedule_api_url):
+        pass
+
+    # Add your methods and attributes
