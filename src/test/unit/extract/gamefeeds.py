8341e82d0e14614b9cbe01bd0dbab5916653160b
Gaurav Gurjar <ggurjar333@gmail.com>
2023-12-18 17:01:59 +0530

    reformatted code.

--- src/test/unit/extract/gamefeeds.py
+++ src/test/unit/extract/gamefeeds.py
@@ -3,8 +3,10 @@ import unittest
 from unittest.mock import patch, MagicMock
 from src.sportsradar.extract.gamefeeds import GameFeeds
 
+
 class TestGameFeeds(unittest.TestCase):
     """Unit tests for GameFeeds class."""
+
     @classmethod
     def setUpClass(cls):
         """Set up for all tests."""
@@ -25,6 +27,7 @@ class TestGameFeeds(unittest.TestCase):
         mock_data_store_instance.fetch_data.assert_called_once_with(
             url="https://testurl.com/api/2023/preseason/3")
         self.assertEqual(result, {"data": "test_data"})
+
 
 # Instruct Python unittest framework to generate a test suite and run the tests.
 if __name__ == "__main__":
