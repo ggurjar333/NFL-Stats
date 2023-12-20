0b5879b64f53733bca98e019b5ba9025bb4bf383
Gaurav Gurjar <ggurjar333@gmail.com>
2023-12-20 17:01:42 +0530

    adds save_data() and testing notebooks folder.

--- src/sportsradar/workspace/datastore.py
+++ src/sportsradar/workspace/datastore.py
@@ -1,37 +1,56 @@
-import os
-
-import requests
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
-
 from urllib3.util.retry import Retry
 from pydantic import HttpUrl
 from requests.adapters import HTTPAdapter
-
+import requests
 from src.sportsradar import logging_helpers
+import os
+from dotenv import load_dotenv
 
 logger = logging_helpers.get_logger(__name__)
 
+load_dotenv('../../../../../.env')
+
+
+def create_mongo_client():
+    mongo_url = os.getenv('MONGODB_URL')
+    if mongo_url is None:
+        raise ValueError("MongoDB environment variable for URL not set.")
+    return MongoClient(host=mongo_url, server_api=ServerApi('1'), port=27017)
+
+
+def setup_http_session():
+    retries = Retry(total=3, backoff_factor=2, status_forcelist=[429, 500, 502, 503, 504])
+    adapter = HTTPAdapter(max_retries=retries)
+    session = requests.Session()
+    session.mount("http://", adapter)
+    session.mount("https://", adapter)
+    return session
+
+
+def save_data(response, db_uri, database, collection):
+    mongo_client = MongoClient(host=db_uri, server_api=ServerApi('1'), port=27017)
+    if database is None:
+        raise ValueError("MongoDB environment variable not set.")
+    else:
+        print(database)
+        db = mongo_client[database]
+        db[collection].insert_one(response.json())
+
 
 class SportsRadarFetcher:
-    """
-    This class is responsible for fetching
-    data from SportsRadar.
-
-    Attributes:
-        timeout (float): The timeout value
-        for HTTP requests.
-        http (requests.Session): The HTTP
-        session to use for requests.
-    """
-
-    def __init__(self, timeout: float = 15):
+    def __init__(self, timeout: float = 30):
         self.timeout = timeout
-        retries = Retry(total=3, backoff_factor=2, status_forcelist=[429, 500, 502, 503, 504])
-        adapter = HTTPAdapter(max_retries=retries)
-        self.http = requests.Session()
-        self.http.mount("http://", adapter)
-        self.http.mount("https://", adapter)
+        self.http = setup_http_session()
+        self.mongo_client = create_mongo_client()
+        self.mongo_db = os.getenv('MONGODB_DATABASE')
+        if self.mongo_db is None:
+            raise ValueError("MongoDB environment variable for Database not set.")
+
+    # def save_data(self, collection, data):
+    #     db = self.mongo_client[self.mongo_db]
+    #     db[collection].insert_one(data)
 
     def _fetch_from_url(self, url: HttpUrl) -> requests.Response:
         logger.info(f"Retrieving {url} from SportsRadar")
@@ -42,25 +61,26 @@ class SportsRadarFetcher:
         raise ValueError(f"Could not download from {url}: {response.text}")
 
     def fetch_data(self, url: HttpUrl) -> requests.Response:
-        return self._fetch_from_url(url)
-
-
-def save_data(response, collection):
-    mongo_client = MongoClient(host=os.environ.get("MONGODB_URL"), server_api=ServerApi('1'))
-    db = mongo_client[os.environ.get("MONGODB_DATABASE")]
-    db[collection].insert_many(response.json())
+        try:
+            return self._fetch_from_url(url)
+        except requests.exceptions.RequestException as e:
+            logger.error(f"Error fetching URL {url}: {str(e)}")
+            raise
 
 
 class DataStore:
-    """
-    This class used to fetch data.
+    def __init__(self, fetcher: SportsRadarFetcher):
+        self.fetcher = fetcher
+        self.data = {}
 
-    Attributes:
-        datakeeper (SportsRadarFetcher): The instance to use for fetching data.
-    """
-
-    def __init__(self, datakeeper):
-        self.datakeeper = datakeeper
-
+    # other methods remain same
     def fetch_data(self, url):
-        return self.datakeeper.fetch_data(url)
+        try:
+            response = self.fetcher.fetch_data(url)
+            if response.status_code == requests.codes.ok:
+                self.data.update({url: response.json()})
+                print(url)
+                return response
+        except requests.exceptions.RequestException as e:
+            logger.error(f"Error fetching URL {url}: {str(e)}")
+            raise
--- src/test/unit/sportsradar/extract/gamefeeds.py
+++ src/test/unit/sportsradar/extract/gamefeeds.py
@@ -1,21 +1,22 @@
 from dotenv import load_dotenv
 import os
+import unittest
+from datetime import datetime
 from src.sportsradar.extract.gamefeeds import GameFeeds
-from src.sportsradar.workspace.datastore import DataStore
+from src.sportsradar.workspace.datastore import save_data
 
 load_dotenv('../../../../../.env')
 
 
-class TestConstants():
+class TestConstants:
     BASE_URL = 'https://api.sportradar.us/nfl/official'
     ACCESS_LEVEL = 'trial'
     VERSION = 'v7'
     LANGUAGE_CODE = 'en'
     FORMAT = 'json'
     API_KEY = f'{os.environ.get("APIKEY")}'
-
-
-import unittest
+    MONGODB_URL = f"{os.environ.get('MONGODB_URL')}"
+    MONGODB_DATABASE = f"{os.environ.get('MONGODB_DATABASE')}"
 
 
 class TestGameFeeds(unittest.TestCase):
@@ -31,29 +32,14 @@ class TestGameFeeds(unittest.TestCase):
                                                    game_id=self.game_id,
                                                    file_format=TestConstants.FORMAT,
                                                    api_key=TestConstants.API_KEY)
-        assert result.status_code == self.expected_status, f"Expected status code {self.expected_status}, but got {result.status_code}."
-
-    def test_get_game_roster(self):
-        # Execute
-        game_feeds = GameFeeds(base_url=TestConstants.BASE_URL)
-        game_id = '251ac0cf-d97d-4fe8-a39e-09fc4a95a0b2'
-        result = game_feeds.get_game_roster(access_level=TestConstants.ACCESS_LEVEL,
-                                            language_code=TestConstants.LANGUAGE_CODE, version=TestConstants.VERSION,
-                                            game_id=game_id, file_format=TestConstants.FORMAT,
-                                            api_key=TestConstants.API_KEY)
-        assert result.status_code == self.expected_status, f"Expected status code {self.expected_status}, but got {result.status_code}."
-
-    def test_get_game_statistics(self):
-        # Execute
-        game_feeds = GameFeeds(base_url=TestConstants.BASE_URL)
-        game_id = '251ac0cf-d97d-4fe8-a39e-09fc4a95a0b2'
-        result = game_feeds.get_game_statistics(access_level=TestConstants.ACCESS_LEVEL,
-                                                language_code=TestConstants.LANGUAGE_CODE,
-                                                version=TestConstants.VERSION,
-                                                game_id=game_id, file_format=TestConstants.FORMAT,
-                                                api_key=TestConstants.API_KEY)
+        if result.status_code == self.expected_status:
+            save_data(response=result,
+                      db_uri=TestConstants.MONGODB_URL,
+                      database=TestConstants.MONGODB_DATABASE,
+                      collection=f'test_game_boxscore_{datetime.now().strftime("%Y%m%d_%H%M%S")}'
+                      )
         assert result.status_code == self.expected_status, f"Expected status code {self.expected_status}, but got {result.status_code}."
 
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(argv=[''], defaultTest='TestGameFeeds')
