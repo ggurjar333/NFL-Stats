3bc5d0d8232bb4d6b3b45caf4a589fbd9adbfa6c
Gaurav Gurjar <ggurjar333@gmail.com>
2023-12-22 20:04:54 +0530

    add transform_boxscore(), transform_game_roster(), transform_game_statistics() and _remove_unwanted_keys() methods.

--- src/sportsradar/transform/gamefeeds.py
+++ src/sportsradar/transform/gamefeeds.py
@@ -5,22 +5,42 @@ logger = logging_helpers.get_logger(__name__)
 
 
 class GameFeedsTransformer:
-    """This class transforms game feeds into structured format"""
+    """
+
+    Class: GameFeedsTransformer
+
+    This class is responsible for transforming game feeds data by removing unwanted keys.
+
+    Attributes:
+    - UNWANTED_KEYS: A list of keys that are unwanted and should be removed from the data.
+
+    Methods:
+    - __init__(self, data: dict): Initializes an instance of the GameFeedsTransformer class with the provided data.
+    - _remove_unwanted_keys(): Removes the unwanted keys from the data.
+    - transform_boxscore(): Transforms the game feeds data for boxscore.
+    - transform_game_roster(): Transforms the game feeds data for game roster.
+    - transform_game_statistics(): Transforms the game feeds data for game statistics.
+
+    """
+
+    UNWANTED_KEYS = ["_comment"]
 
     def __init__(self, data: dict):
         self.data = data
 
+    def _remove_unwanted_keys(self):
+        for key in self.UNWANTED_KEYS:
+            if key in self.data:
+                self.data.pop(key)
+
     def transform_boxscore(self):
-        self.data.pop("_id")
-        self.data.pop("_comment")
+        self._remove_unwanted_keys()
         return self.data
 
     def transform_game_roster(self):
-        self.data.pop("_id")
-        self.data.pop("_comment")
+        self._remove_unwanted_keys()
         return self.data
 
     def transform_game_statistics(self):
-        self.data.pop("_id")
-        self.data.pop("_comment")
+        self._remove_unwanted_keys()
         return self.data
