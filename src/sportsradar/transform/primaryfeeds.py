11f34adc2688716d2c302b6e39b0b8a5380ceaef
Bassey John <jsbazman@gmail.com>
2023-12-24 15:56:53 +0100

    Created a transformation file for primaryfeeds

---
+++ src/sportsradar/transform/primaryfeeds.py
@@ -0,0 +1,44 @@
+from src.sportsradar import logging_helpers
+
+logger = logging_helpers(__name__)
+
+
+class PlayerFeedsTransformer:
+    """
+    Class to transform player feeds data.
+
+    Attributes:
+        UNWANTED_KEYS(list): List of unwanted keys to be removed from the data dictonary.
+
+    Args:
+        data (dict): The game feeds data dictionary.
+
+    Methods:
+
+    """
+
+    UNWANTED_KEYS = ["_comment"]
+
+    def _init__(self, data: dict):
+        self.data = data
+
+    def _remove_unwanted_feeds(self):
+        for key in self.UNWANTED_KEYS:
+            if key in self.data:
+                self.data.pop(key)
+
+    def transform_current_season_schedule(self):
+        self._remove_unwanted_feeds()
+        return self.data
+
+    def transform_current_week_schedule(self):
+        self._remove_unwanted_feeds()
+        return self.data
+
+    def transform_seasons_schedule(self):
+        self._remove_unwanted_feeds()
+        return self.data
+
+    def transform_weekly_schedule(self):
+        self._remove_unwanted_feeds()
+        return self.data
