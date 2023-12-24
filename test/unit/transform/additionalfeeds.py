18db0321d84e0d5b16bfdeadc72cfa8954b676e0
Bassey John <jsbazman@gmail.com>
2023-12-24 05:27:12 +0100

    Added Unit Test functions

--- test/unit/transform/additionalfeeds.py
+++ test/unit/transform/additionalfeeds.py
@@ -29,6 +29,27 @@ class TestAdditionalFeedsTransformer(unittest.TestCase):
         # Assert
         self.assertIsInstance(result, dict)
 
+    def test_transform_daily_change_log(self):
+        # Act
+        result = self.gft.transform_daily_change_log()
+
+        # Assert
+        self.assertIsInstance(result, dict)
+
+    def test_transform_daily_transactions(self):
+        # Act
+        result = self.gft.transform_daily_transactions()
+
+        # Assert
+        self.assertIsInstance(result, dict)
+
+    def test_transform_seasoms(self):
+        # Act
+        result = self.gft.transform_seasons()
+
+        # Assert
+        self.assertIsInstance(result, dict)
+
 
 if __name__ == "__main__":
     unittest.main(argv=[""], defaultTest="TestAdditionalFeedsTransformer")
